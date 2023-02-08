f1df3ce2078f49ea59264aff665c2c4b52e07aaf
Mike Yang <mikeyang@Mikes-MacBook-Air.local>
2023-02-08 22:48:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+    const [todos, setTodos] = useState([
+      "Read a book",
+      "Write a blog",
+      "Make a video",
+      "Reply a comment",
+    ]);
+
+    const [todo, setToDo] = useState('');
+    const handleClick = () => {
+      if(!todo){
+        return;
+      }
+      // const todo = 'Like this video';
+      const newTodos = todos.concat([todo]);
+      setTodos(newTodos);
+      setToDo('');
+   };
+
+    const handleChange = (e) => {
+      const todo = e.target.value;
+      setToDo(todo);
+      console.log({todo});
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todo List</h1>
+      <div>
+        <input type="text" onChange={handleChange} value={todo} />
+          <button onClick={handleClick}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo) => {
+          return <li>{todo}</li>;
+        })}
+      </ul>
     </div>
   );
 }
